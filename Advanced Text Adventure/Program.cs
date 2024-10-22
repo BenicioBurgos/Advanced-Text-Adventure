a3446f8ac876ada251b12686f420cb709a709b2b
Burgos <1718014@mukilteo.wednet.edu>
2024-10-22 10:08:29 -0700

    push

--- Advanced Text Adventure/Program.cs
+++ Advanced Text Adventure/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Advanced_Text_Adventure
+﻿using System.Drawing;
+using System.IO;
+
+namespace Advanced_Text_Adventure
 {
     internal class Program
     {
+        public static string[] shades = [" ", "░", "▒", "▓", "█"];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int troll = 0; troll < 100; troll++)
+                DrawImage("image");
+        }
+
+        public static void DrawImage(string name)
+        {
+            Bitmap image = new(Directory.GetCurrentDirectory() + "/Images/" + name + ".png");
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color color = image.GetPixel(x, y);
+                    int lightLevel = (int)MathF.Round(MathF.Max(MathF.Max(color.R, color.G), color.B) / 255f * (shades.Length - 1));
+                    Console.Write(shades[lightLevel]);
+                    Console.Write(shades[lightLevel]);
+                }
+                Console.Write(" \r\n");
+            }
         }
     }
 }
