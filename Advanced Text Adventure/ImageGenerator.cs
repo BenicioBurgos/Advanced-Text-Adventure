7a2fc14a15dd09a5919d4df8025715c0b8c9c6fe
brunger7 <133077365+brunger7@users.noreply.github.com>
2024-10-29 22:37:05 -0700

    man

---
+++ Advanced Text Adventure/ImageGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+
+namespace Advanced_Text_Adventure
+{
+    internal class ImageGenerator
+    {
+        static readonly string[] shades = [" ", "░", "▒", "▓", "█"];
+        static readonly int[] consoleHues = [354, 54, 116, 180, 221, 307];
+
+        public static void DrawImage(string name)
+        {
+            Bitmap image = new(Directory.GetCurrentDirectory() + "/Images/" + name + ".png");
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color color = image.GetPixel(x, y);
+                    int saturation = (int)MathF.Round((1 - MathF.Min(MathF.Min(color.R, color.G), color.B) / 255f) * (shades.Length - 1));
+                    float value = MathF.Max(MathF.Max(color.R, color.G), color.B) / 255f;
+                    if (MathF.Max(MathF.Max(color.R, color.G), color.B) < 100)
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    else
+                        Console.ForegroundColor = GetColor(color.GetHue(), value < 0.7f);
+                    Console.Write(shades[saturation]);
+                    Console.Write(shades[saturation]);
+                }
+                Console.Write(" \r\n");
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public static ConsoleColor GetColor(float hue, bool dark)
+        {
+            if (!dark)
+            {
+                if (hue >= 330 || hue <= 24)
+                    return ConsoleColor.Red;
+                else if (hue >= 24 && hue <= 85)
+                    return ConsoleColor.Yellow;
+                else if (hue >= 85 && hue <= 148)
+                    return ConsoleColor.Green;
+                else if (hue >= 148 && hue <= 200)
+                    return ConsoleColor.Cyan;
+                else if (hue >= 200 && hue <= 264)
+                    return ConsoleColor.Blue;
+                else
+                    return ConsoleColor.Magenta;
+            }
+            else
+            {
+                if (hue >= 330 || hue <= 24)
+                    return ConsoleColor.DarkRed;
+                else if (hue >= 24 && hue <= 85)
+                    return ConsoleColor.DarkYellow;
+                else if (hue >= 85 && hue <= 148)
+                    return ConsoleColor.DarkGreen;
+                else if (hue >= 148 && hue <= 200)
+                    return ConsoleColor.DarkCyan;
+                else if (hue >= 200 && hue <= 264)
+                    return ConsoleColor.DarkBlue;
+                else
+                    return ConsoleColor.DarkMagenta;
+            }
+        }
+    }
+}
--- Advanced Text Adventure/ManiaConverter.cs
+++ Advanced Text Adventure/ManiaConverter.cs
@@ -1,87 +1,90 @@
 ﻿using Advanced_Text_Adventure;
-using System;
+using System.IO;
+using System.IO.Compression;
 
 public class ManiaConverter
 {
+    public static string activeSongPath;
+
     public static void Convert()
     {
-        Console.SetIn(new StreamReader(Console.OpenStandardInput(131072), Console.InputEncoding, false, 131072));
-        Console.WriteLine("Input note data.");
-        string data = Console.ReadLine();
-        Console.Clear();
         List<int> timings = [];
         List<int> lanes = [];
         List<int> holds = [];
-        int phase = 0;
-        string timing = "";
-        string lane = "";
-        string hold = "";
-        for (int chara = 0; chara < data.Length; chara++)
+        string zipPath = Console.ReadLine().Replace("\"", "");
+        string folderName = zipPath.Split("\\")[^1];
+        ZipFile.ExtractToDirectory(zipPath, Directory.GetCurrentDirectory() + "/Data/" + folderName, true);
+        activeSongPath = Directory.GetCurrentDirectory() + "/Data/" + folderName;
+        int diff = int.Parse(Console.ReadLine());
+        Console.WriteLine("Loading...");
+        List<string> dataList = [.. File.ReadAllText(Directory.GetFiles(activeSongPath, "*.osu")[diff]).Split("\r\n")];
+        int l = dataList.IndexOf("[HitObjects]");
+        for (int i = 0; i <= l; i++)
+            dataList.RemoveAt(0);
+        foreach (string line in dataList)
         {
-            Console.SetCursorPosition(0, 0);
-            Console.Write("Loading: " + MathF.Round(chara / (float)data.Length * 100) + "%");
-            switch (phase)
+            int phase = 0;
+            string timing = "";
+            string lane = "";
+            string hold = "";
+            for (int chara = 0; chara < line.Length; chara++)
             {
-                case 0:
-                    if (data[chara] != ',')
-                        lane += data[chara];
-                    else
-                    {
-                        phase = 1;
-                        if (lane == "64")
-                            lane = "0";
-                        else if (lane == "192")
-                            lane = "1";
-                        else if (lane == "320")
-                            lane = "2";
-                        else if (lane == "448")
-                            lane = "3";
-                        lanes.Add(int.Parse(lane));
-                    }
-                    break;
-                case 1:
-                    if (data[chara] == ',')
-                        phase = 2;
-                    break;
-                case 2:
-                    if (data[chara] != ',')
-                        timing += data[chara];
-                    else
-                    {
-                        phase = 3;
-                        timings.Add(int.Parse(timing));
-                    }
-                    break;
-                case 3: case 4:
-                    if (data[chara] == ',')
-                        phase++;
-                    break;
-                case 5:
-                    if (data[chara] != ':')
-                        hold += data[chara];
-                    else
-                    {
-                        phase = 6;
-                        if (hold != "0")
-                            holds.Add(int.Parse(hold) - int.Parse(timing));
+                Console.SetCursorPosition(0, 0);
+                switch (phase)
+                {
+                    case 0:
+                        if (line[chara] != ',')
+                            lane += line[chara];
                         else
-                            holds.Add(0);
-                    }
-                    break;
-                case 6:
-                    if (data[chara] == '|')
-                    {
-                        timing = "";
-                        lane = "";
-                        hold = "";
-                        phase = 0;
-                    }
-                    break;
+                        {
+                            phase = 1;
+                            if (lane == "64")
+                                lane = "0";
+                            else if (lane == "192")
+                                lane = "1";
+                            else if (lane == "320")
+                                lane = "2";
+                            else if (lane == "448")
+                                lane = "3";
+                            lanes.Add(int.Parse(lane));
+                        }
+                        break;
+                    case 1:
+                        if (line[chara] == ',')
+                            phase = 2;
+                        break;
+                    case 2:
+                        if (line[chara] != ',')
+                            timing += line[chara];
+                        else
+                        {
+                            phase = 3;
+                            timings.Add(int.Parse(timing));
+                        }
+                        break;
+                    case 3: case 4:
+                        if (line[chara] == ',')
+                            phase++;
+                        break;
+                    case 5:
+                        if (line[chara] != ':')
+                            hold += line[chara];
+                        else
+                        {
+                            if (hold != "0")
+                                holds.Add(int.Parse(hold) - int.Parse(timing));
+                            else
+                                holds.Add(0);
+                            goto end;
+                        }
+                        break;
+                }
             }
+        end:;
         }
         Program.noteTimes = timings;
         Program.noteLanes = lanes;
-        Program.noteHolds = lanes;
+        Program.noteHolds = holds;
         Console.Clear();
     }
 }
